26e5dfa017cfb26567eaa37982acb6e0d4f1e292
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2023-05-13 01:19:53 +0100

    Add files via upload

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+Use Symfony\Component\Routing\Annotation\Route;
+class IndexController extends AbstractController
+{
+ 
+ public function home($name)
+ {
+ return $this->render('index.html.twig',['name' => $name]);
+ }
+}
